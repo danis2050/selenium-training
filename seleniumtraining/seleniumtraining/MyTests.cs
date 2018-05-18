a65abcd1eeace76351060582912ca7c6a81b9051
Danis Gayazov <danis2050@hotmail.com>
2018-05-19 01:36:46 +0300

    seleniumtraining project
    проект seleniumtraiing, google test

---
+++ seleniumtraining/seleniumtraining/MyTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+
+namespace seleniumtraining
+{
+    [TestFixture]
+    public class MyTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            
+            driver.Navigate().GoToUrl("http://www.google.ru/");
+            driver.FindElement(By.Id("lst-ib")).SendKeys("software-testing.ru");
+            driver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+           
+        }
+
+        [TearDown]
+
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ seleniumtraining/seleniumtraining/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("seleniumtraining")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("seleniumtraining")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ae29a42-5bb2-43da-bff1-38261e2d63a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
